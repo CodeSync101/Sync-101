7f65f7408a771787544594d86f8f399796db6a0e
FetenDridi <dfaten185@gmail.com>
2025-05-29 12:49:45 +0100

    tickets

--- src/main/java/feten/tn/projetmission/Controllers/TicketController.java
+++ src/main/java/feten/tn/projetmission/Controllers/TicketController.java
@@ -2,35 +2,37 @@ package feten.tn.projetmission.Controllers;
 import feten.tn.projetmission.Entities.Tache;
 import feten.tn.projetmission.Entities.Ticket;
 import feten.tn.projetmission.Services.NoteService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
-
+@AllArgsConstructor
+@CrossOrigin("*")
 @RestController
-@RequestMapping("/api/notes")
+@RequestMapping("/Ticket")
 public class TicketController {
 
     @Autowired
     private NoteService noteService;
 
-    @PostMapping("/addNoteticket/{tacheId}")
+    @PostMapping("/addticket/{tacheId}")
     public ResponseEntity<Ticket> addNote(@PathVariable Long tacheId, @RequestBody Ticket ticket) {
         return ResponseEntity.ok(noteService.addOrUpdateTicketNote(tacheId, ticket));
     }
 
-    @PutMapping("/updateNoteticket/{tacheId}/{ticketId}")
+    @PutMapping("/updateticket/{tacheId}/{ticketId}")
     public ResponseEntity<Ticket> updateNote(@PathVariable Long tacheId, @PathVariable Long ticketId, @RequestBody Ticket ticket) {
         ticket.setId(ticketId);
         return ResponseEntity.ok(noteService.addOrUpdateTicketNote(tacheId, ticket));
     }
 
-    @DeleteMapping("/deleteNoteticket/{ticketId}")
+    @DeleteMapping("/deleteticket/{ticketId}")
     public ResponseEntity<Void> deleteNote(@PathVariable Long ticketId) {
         noteService.deleteTicket(ticketId);
         return ResponseEntity.noContent().build();
     }
-    @GetMapping("/getNotestickets/{tacheId}")
+    @GetMapping("/getTickets/{tacheId}")
     public ResponseEntity<List<Ticket>> getTickets(@PathVariable Long tacheId) {
         return ResponseEntity.ok(noteService.getTicketsByTache(tacheId));
     }
